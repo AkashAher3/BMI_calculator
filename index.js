cc44fc8756e69b646f0792a7a849dba9b83e13cf
Akash Aher <akashnaher2018@gmail.com>
2023-10-03 10:51:23 +0530

    new Project

---
+++ index.js
@@ -0,0 +1,23 @@
+const weight=document.getElementById("wet").value;
+const hfeet=document.getElementById("feet").value;
+const inches=document.getElementById("inches").value;
+const button =document.querySelector("button");
+const bmiscore=document.getElementById("bmi")
+const feettoinc=hfeet*12
+let add=feettoinc+Number(inches)
+let meterconversan=add*0.0254
+let metersquare=meterconversan*meterconversan
+let finalBMI=weight/metersquare
+
+
+button.addEventListener('click',function(){
+    let result=(finalBMI.toFixed(2))
+    bmiscore.textContent=`${result}`
+    console.log(result)
+    
+    
+    
+})
+
+
+
